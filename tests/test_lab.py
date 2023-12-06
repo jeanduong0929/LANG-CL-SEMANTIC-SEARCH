d91f216b43e93ec416e200c38d1e2bc08d09444c
bduong <bduong@accuregsoftware.com>
2023-12-06 16:45:48 -0500

    Add tests for get_embeddings_from_csv and get_chromadb_collection

--- tests/test_lab.py
+++ tests/test_lab.py
@@ -5,32 +5,69 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 import unittest  # noqa: E402
 from unittest.mock import MagicMock, patch  # noqa: E402
-from src.app import get_chromadb_collection  # noqa: E402
+from src.app import get_chromadb_collection, get_embeddings_from_csv  # noqa: E402
 
 
 class ChromaDBTests(unittest.TestCase):
-    def test_get_chromadb_collection(self):
-        """
-        Tests the get_chromadb_collection function. This test checks if the function correctly
-        handles existing collections and creates a new one as needed.
+    def test_get_embeddings_from_csv_valid_data(self):
+        test_data = [["ID", "Text"], ["1", "Document 1"], ["2", "Document 2"]]
+        expected_documents = ["Document 1", "Document 2"]
+        expected_metadata = [{"item_id": "1"}, {"item_id": "2"}]
+        expected_ids = ["1", "2"]
 
-        - Mocks the chromadb.Client to simulate the presence of an existing 'test' collection.
-        - Verifies that the existing collection is deleted.
-        - Confirms that a new collection is created.
-        - Asserts that the function returns the expected value after creating the new collection.
+        documents, metadata, ids = get_embeddings_from_csv(test_data)
 
-        This ensures that the function behaves correctly when managing collections in the database.
-        """
+        self.assertEqual(documents, expected_documents)
+        self.assertEqual(metadata, expected_metadata)
+        self.assertEqual(ids, expected_ids)
+
+    def test_get_embeddings_from_csv_incomplete_data(self):
+        # Set the test data
+        test_data = [["ID", "Text"], ["1"], ["2", "Document 2"]]
+
+        # Set the expected return values
+        expected_documents = ["Document 2"]
+        expected_metadata = [{"item_id": "2"}]
+        expected_ids = ["2"]
+
+        # Call the function to get the actual values
+        actual_documents, actual_metadata, actual_ids = get_embeddings_from_csv(
+            test_data
+        )
+
+        # Assert the actual values are the same as the expected values
+        self.assertEqual(actual_documents, expected_documents)
+        self.assertEqual(actual_metadata, expected_metadata)
+        self.assertEqual(actual_ids, expected_ids)
+
+    @patch("src.app.chromadb.Client")
+    def test_get_chromadb_collection(self, mock_chromadb_client):
+        # Mock the ChromaDB client and collection
         mock_client = MagicMock()
-        mock_client.list_collections.return_value = ["test"]
-        mock_client.create_collection.return_value = "Collection Created"
+        mock_collection = MagicMock()
 
-        with patch("chromadb.Client", return_value=mock_client):
-            collection = get_chromadb_collection()
+        # Set the return values for the mocked functions
+        mock_chromadb_client.return_value = mock_client
+        mock_client.create_collection.return_value = mock_collection
 
-            mock_client.delete_collection.assert_called_with(name="test")
-            mock_client.create_collection.assert_called_with(name="test")
-            self.assertEqual(collection, "Collection Created")
+        # Set the test data
+        documents = ["Document 1", "Document 2"]
+        metadatas = [{"item_id": "1"}, {"item_id": "2"}]
+        ids = ["1", "2"]
+
+        # Call the function to get the actual values
+        actual_collection = get_chromadb_collection(documents, metadatas, ids)
+
+        # Assert of actual_collection is the same as the mocked collection
+        self.assertEqual(actual_collection, mock_collection)
+
+        # Assert the mocked functions were called with the correct parameters
+        mock_client.create_collection.assert_called_with(name="semantic-lab")
+
+        # Assert the mocked functions were called with the correct parameters
+        mock_collection.add.assert_called_with(
+            documents=documents, metadatas=metadatas, ids=ids
+        )
 
 
 if __name__ == "__main__":
